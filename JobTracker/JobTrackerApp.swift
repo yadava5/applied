1b5f6c513ebf712920867a01bfa0ad63fc04c9a5
Ayush Yadav <yadava5@miamioh.edu>
2026-02-10 09:40:50 -0500

    Initial Commit

---
+++ JobTracker/JobTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobTrackerApp.swift
+//  JobTracker
+//
+//  Created by Ayush Yadav on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct JobTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
